fec24889967f57f4ab09c68b0390fa1fc360718e
Crispin Wellington <retrogradeorbit@gmail.com>
2019-10-14 23:41:27 +0800

    improve project definition

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bootleg "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple template processing command line tool to help build static websites"
+  :url "https://github.com/retrogradeorbit/bootleg"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
