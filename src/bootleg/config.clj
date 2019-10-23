fe82f581f74129789c405ea36be4f7f3d9bf8e63
Crispin Wellington <retrogradeorbit@gmail.com>
2019-10-23 12:23:14 +0800

    fix for issue #1

--- src/bootleg/config.clj
+++ src/bootleg/config.clj
@@ -1,19 +1,14 @@
 (ns bootleg.config
   (:require [bootleg.file :as file]
             [clojure.java.io :as io])
-  (:import [java.util Base64]
-           ))
-
-(def home-dir (System/getenv "HOME"))
-(def config-dir (file/path-join home-dir ".bootleg"))
-(def libs-dir (file/path-join config-dir "libs"))
+  (:import [java.util Base64]))
 
 (def libs-set ["sunec.lib" "sunec.dll" "libsunec.dylib" "libsunec.so"])
 
 (defn setup
   "Copy any of the bundled dynamic libs from resources to the
   run time lib directory"
-  []
+  [libs-dir]
   (doseq [filename libs-set]
     (when-let [file (io/resource filename)]
       (let [[_ name] (file/path-split (.getFile file))]
@@ -23,10 +18,13 @@
 (defn init! []
   (let [native-image?
         (and (= "Substrate VM" (System/getProperty "java.vm.name"))
-             (= "runtime" (System/getProperty "org.graalvm.nativeimage.imagecode")))]
+             (= "runtime" (System/getProperty "org.graalvm.nativeimage.imagecode")))
+        home-dir (System/getenv "HOME")
+        config-dir (file/path-join home-dir ".bootleg")
+        libs-dir (file/path-join config-dir "libs")]
     (.mkdirs (io/as-file libs-dir))
 
     (when native-image?
       ;; https://blog.taylorwood.io/2018/10/04/graalvm-https.html
-      (setup)
+      (setup libs-dir)
       (System/setProperty "java.library.path" libs-dir))))
