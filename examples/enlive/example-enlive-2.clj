b055a527e321899b7ab04426ca9be5f8d70a399e
Crispin Wellington <retrogradeorbit@gmail.com>
2019-10-27 21:17:06 +0800

    emlive examples

--- examples/enlive/example-enlive-2.clj
+++ examples/enlive/example-enlive-2.clj
@@ -1,8 +1,17 @@
 (require '[net.cgrand.enlive-html :as html])
 
+(html/deftemplate main-template "templates/application.html" [context]
+  [:head :title] (html/content (:heading context)))
 
-(html/deftemplate main-template "templates/application.html"
-  []
-  [:head :title] (html/content "Enlive starter kit"))
+(html/defsnippet main-snippet "templates/header.html" [:header] [heading navigation-elements]
+  [:h1] (html/content heading)
+  [:ul [:li html/first-of-type]] (html/clone-for [[caption url] navigation-elements]
+                                                 [:li :a] (html/content caption)
+                                                 [:li :a] (html/set-attr :href url)))
 
-[:div]
+#_ (->> (main-template {:heading "a new heading"})
+     ;;html/emit*
+     (apply str)
+    )
+
+(apply str (main-template {:heading (main-snippet "heading" [["caption" "url"] ["caption 2" "url 2"]])}))
