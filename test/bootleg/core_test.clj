fa84666999b06730409b46bd51601ba454f0e304
Crispin Wellington <retrogradeorbit@gmail.com>
2020-01-25 23:46:38 +0800

    yaml and json :data tests issue #45

--- test/bootleg/core_test.clj
+++ test/bootleg/core_test.clj
@@ -1,7 +1,9 @@
 (ns bootleg.core-test
   (:require [clojure.test :refer :all]
             [bootleg.core :refer :all]
-            [bootleg.hiccup :refer :all]))
+            [bootleg.hiccup :refer :all]
+            [bootleg.yaml :refer :all]
+            [bootleg.json :refer :all]))
 
 (deftest slurp-test
   (testing "test slurp returns string"
@@ -17,3 +19,25 @@
            (process-hiccup-data
             "./test/files/"
             "(slurp \"https://raw.githubusercontent.com/retrogradeorbit/bootleg/master/test/files/simple.md\")")))))
+
+(deftest yaml-test
+  (testing "test yaml function"
+    (is (= (yaml "
+foo: bar
+baz:
+ - one
+ - two
+" :data)
+           {:foo "bar"
+            :baz ["one" "two"]}))))
+
+(deftest json-test
+  (testing "test json function"
+    (is (= (json "
+{
+  \"foo\": \"bar\",
+  \"baz\": [\"one\", \"two\"]
+}
+" :data)
+           {:foo "bar"
+            :baz ["one" "two"]}))))
