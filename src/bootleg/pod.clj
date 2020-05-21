8afc5dadfec47417828abc3a4ae02e719a8398aa
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-21 13:18:26 +0800

    more funcs and macro processor

--- src/bootleg/pod.clj
+++ src/bootleg/pod.clj
@@ -11,7 +11,33 @@
             [bootleg.yaml]
             [bootleg.json]
             [bootleg.edn]
-            [bootleg.file])
+            [bootleg.file]
+            [bootleg.enlive]
+
+            [hickory.convert]
+            [hickory.hiccup-utils]
+            [hickory.render]
+            [hickory.select]
+            [hickory.utils]
+            [hickory.zip]
+
+            [net.cgrand.enlive-html]
+            [net.cgrand.jsoup]
+            [net.cgrand.tagsoup]
+            [net.cgrand.xml]
+
+            [selmer.filter-parser]
+            [selmer.filters]
+            [selmer.middleware]
+            [selmer.node]
+            [selmer.parser]
+            [selmer.tags]
+            [selmer.template-parser]
+            [selmer.util]
+            [selmer.validator]
+
+            [clojure.repl]
+            )
   (:import [java.io PushbackInputStream])
   )
 
@@ -21,7 +47,7 @@
   (write-bencode System/out v)
   (.flush System/out))
 
-(defmacro make-looup [ns]
+(defmacro make-lookup [ns]
   (->>
    (for [[k v] (ns-publics ns)]
      (let [{:keys [ns name macro]} (meta v)]
@@ -45,18 +71,64 @@
 
 #_ (macroexpand-1 '(make-namespace-def bootleg.glob))
 
+(def translate-ns
+  {"bootleg.utils" "pod.retrogradeorbit.bootleg.utils"
+   "net.cgrand.enlive-html" "pod.retrogradeorbit.net.cgrand.enlive-html"})
+
+(defmacro process-macro-source [sym]
+  (->
+   (clojure.repl/source-fn sym)
+   (edamame.core/parse-string  {:all true})
+   (->> (clojure.walk/postwalk
+         (fn [form]
+           (if (symbol? form)
+             (let [ns (namespace form)
+                   sym-name (name form)]
+               (if-let [translated-ns (translate-ns ns)]
+                 (symbol translated-ns sym-name)
+                 (symbol ns sym-name)))
+             form))))
+   prn-str))
+
+(bootleg.utils/pprint (process-macro-source bootleg.enlive/deftemplate))
+
 (def lookup (merge
-             (make-looup bootleg.glob)
-             (make-looup bootleg.utils)
-             (make-looup bootleg.markdown)
-             (make-looup bootleg.mustache)
-             (make-looup bootleg.html)
-             (make-looup bootleg.hiccup)
-             (make-looup bootleg.selmer)
-             (make-looup bootleg.yaml)
-             (make-looup bootleg.json)
-             (make-looup bootleg.edn)
-             (make-looup bootleg.file)))
+             (make-lookup bootleg.glob)
+             (make-lookup bootleg.utils)
+             (make-lookup bootleg.markdown)
+             (make-lookup bootleg.mustache)
+             (make-lookup bootleg.html)
+             (make-lookup bootleg.hiccup)
+             (make-lookup bootleg.selmer)
+             (make-lookup bootleg.yaml)
+             (make-lookup bootleg.json)
+             (make-lookup bootleg.edn)
+             (make-lookup bootleg.file)
+             (make-lookup bootleg.enlive)
+
+             (make-lookup hickory.convert)
+             (make-lookup hickory.hiccup-utils)
+             (make-lookup hickory.render)
+             (make-lookup hickory.select)
+             (make-lookup hickory.utils)
+             (make-lookup hickory.zip)
+
+             (make-lookup net.cgrand.enlive-html)
+             (make-lookup net.cgrand.jsoup)
+             (make-lookup net.cgrand.tagsoup)
+             (make-lookup net.cgrand.xml)
+
+             (make-lookup selmer.filter-parser)
+             (make-lookup selmer.filters)
+             (make-lookup selmer.middleware)
+             (make-lookup selmer.node)
+             (make-lookup selmer.parser)
+             (make-lookup selmer.tags)
+             (make-lookup selmer.template-parser)
+             (make-lookup selmer.util)
+             (make-lookup selmer.validator)
+
+             ))
 
 (defn main []
   (try
@@ -64,7 +136,7 @@
       (let [{:strs [id op var args]} (read-bencode stdin)]
 
         #_ (with-open [wrtr (io/writer "./pod-in.txt" :append true)]
-          (.write wrtr (prn-str (String. op))))
+             (.write wrtr (prn-str (String. op))))
 
         (case (String. op)
           "describe" (do (write {"format" "edn"
@@ -79,6 +151,36 @@
                                                (make-namespace-def bootleg.json)
                                                (make-namespace-def bootleg.edn)
                                                (make-namespace-def bootleg.file)
+                                               (make-namespace-def bootleg.enlive)
+                                               (make-namespace-def hickory.convert)
+                                               (make-namespace-def hickory.hiccup-utils)
+                                               (make-namespace-def hickory.render)
+                                               (make-namespace-def hickory.select)
+                                               (make-namespace-def hickory.utils)
+                                               (make-namespace-def hickory.zip)
+
+                                               (make-namespace-def hickory.convert)
+                                               (make-namespace-def hickory.hiccup-utils)
+                                               (make-namespace-def hickory.render)
+                                               (make-namespace-def hickory.select)
+                                               (make-namespace-def hickory.utils)
+                                               (make-namespace-def hickory.zip)
+
+                                               (make-namespace-def net.cgrand.enlive-html)
+                                               (make-namespace-def net.cgrand.jsoup)
+                                               (make-namespace-def net.cgrand.tagsoup)
+                                               (make-namespace-def net.cgrand.xml)
+
+                                               (make-namespace-def selmer.filter-parser)
+                                               (make-namespace-def selmer.filters)
+                                               (make-namespace-def selmer.middleware)
+                                               (make-namespace-def selmer.node)
+                                               (make-namespace-def selmer.parser)
+                                               (make-namespace-def selmer.tags)
+                                               (make-namespace-def selmer.template-parser)
+                                               (make-namespace-def selmer.util)
+                                               (make-namespace-def selmer.validator)
+
                                                ]
                                  "id" (String. id)})
                          (recur))
