b5364692acf4d9ecded07aca0466538de4cb9bf5
Martin Klepsch <martinklepsch@googlemail.com>
2020-05-30 23:22:49 +0200

    expose enlive/at*

--- src/bootleg/enlive.clj
+++ src/bootleg/enlive.clj
@@ -10,6 +10,8 @@
   [path loader]
   (-> path file/path-relative io/input-stream loader))
 
+(def at* net.cgrand.enlive-html/at*)
+
 (defmacro at [node-or-nodes & rules]
   `(let [input-type# (or (bootleg.utils/markup-type ~node-or-nodes) :hickory)
          converted-nodes# (bootleg.utils/convert-to ~node-or-nodes :hickory-seq)]
