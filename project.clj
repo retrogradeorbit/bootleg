ec7a03d672efc674cb91b3f86203c1ff64979094
Crispin Wellington <retrogradeorbit@gmail.com>
2022-02-24 23:11:04 +0800

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,21 +3,21 @@
   :url "https://github.com/retrogradeorbit/bootleg"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
-                 [org.clojure/tools.cli "0.4.2"]
-                 [org.clojure/data.json "0.2.6"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.cli "1.0.206"]
+                 [org.clojure/data.json "2.4.0"]
                  [org.clojure/data.xml "0.2.0-alpha6"]
-                 [markdown-clj "1.10.0"]
+                 [markdown-clj "1.10.8"]
 
-                 [io.forward/yaml "1.0.10"]
+                 [io.forward/yaml "1.0.11" :exclusions [org.yaml/snakeyaml org.flatland/ordered]]
 
-                 [cljstache "2.0.4"]
-                 [enlive "1.1.6"]
-                 [hickory "0.7.1"]
+                 [cljstache "2.0.6"]
+                 [enlive "1.1.6" :exclusions [org.jsoup/jsoup]]
+                 [hickory "0.7.1" :exclusions [org.clojure/tools.reader]]
                  [selmer "1.12.17"]
 
                  ;; for clojure eval
-                 [borkdude/sci "0.0.13-alpha.24"]
+                 [org.babashka/sci "0.2.8"]
 
                  ;; clojures pprint doesn't work under graal native-image
                  [fipp "0.6.23"]
